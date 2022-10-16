727ecee05fd81a2247ea5bb327c85348ea089f5a
edison <edisonquu@gmail.com>
2022-10-16 00:56:54 -0400

    changed some of the react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import {useState} from 'react';
 import './App.css';
+import MainMint from './MainMint';
+import NavBar from './NavBar'
+
+
 
 function App() {
+
+  const [accounts, setAccounts] = useState([]);
   return (
     <div className="App">
     
